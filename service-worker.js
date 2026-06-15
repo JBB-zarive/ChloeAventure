d1a65a87ff3d5c4f5886efbc071c8aed7e99adb0
JBB-zarive <76131090+JBB-zarive@users.noreply.github.com>
2026-06-15 16:33:55 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
  * Cache + notifications programmées
  */
 
-const CACHE_NAME = 'chloe-aventure-v5';
+const CACHE_NAME = 'chloe-aventure-v6';
 const STATIC_ASSETS = [
   './', './index.html', './style.css', './app.js', './api.js',
   './manifest.json', './icons/icon-192.png', './icons/icon-512.png',
